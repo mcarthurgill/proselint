5fc0b9742bacec3e4744aaf11a87057927d04bf6
mcarthurgill <mcarthur.gill@gmail.com>
2017-05-17 13:02:36 -0500

    cache with PID

--- proselint/tools.py
+++ proselint/tools.py
@@ -15,7 +15,8 @@ import re
 import hashlib
 import json
 import importlib
-from tempfile import gettempdir
+from datetime import datetime
+import tempfile
 
 try:
     import dbm
@@ -83,14 +84,19 @@ def _get_cache(cachepath):
 def memoize(f):
     """Cache results of computations on disk."""
     # Determine the location of the cache.
-    cache_dirname = os.path.join(gettempdir(), ".proselint")
+    pid = str(os.getpid())
+    cache_dirname_prefix = "proselint_{0}_{1}_".format(datetime.now().strftime("%Y-%m-%d"), pid)
+    cache_dirname = None
 
-    # Create the cache if it does not already exist.
-    if not os.path.isdir(cache_dirname):
-        os.mkdir(cache_dirname)
+    for dir in os.listdir(tempfile.gettempdir()):
+        if dir.startswith(cache_dirname_prefix):
+            cache_dirname = dir
+
+    if cache_dirname is None:
+        cache_dirname = tempfile.mkdtemp(prefix=cache_dirname_prefix)
 
     cache_filename = f.__module__ + "." + f.__name__
-    cachepath = os.path.join(cache_dirname, cache_filename)
+    cachepath = os.path.join(tempfile.gettempdir(), cache_dirname, cache_filename)
 
     @functools.wraps(f)
     def wrapped(*args, **kwargs):
@@ -111,7 +117,7 @@ def memoize(f):
         try:
             cache = _get_cache(cachepath)
             return cache[key]
-        except KeyError:
+        except (KeyError, pickle.UnpicklingError):
             value = f(*args, **kwargs)
             cache[key] = value
             cache.sync()
